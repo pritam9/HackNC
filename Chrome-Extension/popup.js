e5d771fbdaf96fbb8343569be0be5e55056a51fb
Pritam <pborate@uncc.edu>
2016-10-30 10:26:52 -0400

    First Commint
    Hack NC

---
+++ Chrome-Extension/popup.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var checkPageButton = document.getElementById('checkPage');
+  checkPageButton.addEventListener('click', function() {
+
+     chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function(tabs) {
+       var url = tabs[0].url;
+	  // d = document;
+	  var a = 'amazon.com';
+	  var keyw = 'keywords';
+	  
+	  if (url.indexOf(a)!==-1 && url.indexOf(keyw)!==-1)
+	  {
+		  url = url+'&low-price=&high-price=100';
+	  }
+	  
+	  var e='ebay.com';
+	  
+	   
+	  if (url.indexOf(e)!==-1)
+	  {
+		  url = url+'&_udlo&_udhi=100';
+	  }
+	  
+	  
+      // var f = d.createElement('form');
+      // f.action = url;
+      // f.method = 'get';
+      // var i = d.createElement('input');
+      // i.type = 'hidden';
+      // i.name = 'url';
+      // i.value = tabs.url;
+      // f.appendChild(i);
+      // d.body.appendChild(f);
+      // f.submit();
+	  chrome.tabs.update(tabs[0].id, {url: url});
+     });
+	
+  }, false);
+}, false);
